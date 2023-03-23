cc10bae9e3f9fc23bec0e2d9dbd0069d447af2f6
lmmay0 <120409038+lmmay0@users.noreply.github.com>
2023-03-23 11:41:48 -0500

    Folder moved locally to fix git pages to work

---
+++ src/componets/Footer.js
@@ -0,0 +1,18 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function Footer() {
+  return (
+    <Container>
+      <Row>
+        <Col>1 of 1</Col>
+        <p>
+        Email: Lmaynard92@gmail.com
+        </p>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
