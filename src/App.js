3d5c991b3ebcaaa9b832569c20b5b1006111f40d
lmmay0 <120409038+lmmay0@users.noreply.github.com>
2023-03-23 00:50:21 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './componets/Header';
+import Footer from './componets/Footer';
+import Body from './componets/Body';
+function App() {
+  return (
+    <>
+    <Header/>
+    <Body/>
+    <Footer/>
+
+    </>
+  );
+}
+
+export default App;
