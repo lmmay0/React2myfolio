fc5957031e6c946673b9eaf945cf8a8ab76fcae5
lmmay0 <120409038+lmmay0@users.noreply.github.com>
2023-03-23 23:58:02 -0500

    about me

--- src/componets/Footer.js
+++ src/componets/Footer.js
@@ -9,6 +9,7 @@ function Footer() {
         <Col>1 of 1</Col>
         <p>
         Email: Lmaynard92@gmail.com
+       About me: My name is Marvin Maynard and I am a game designer and web developer originally from Maryland. Thank you for look at my portfolio!
         </p>
       </Row>
     </Container>
